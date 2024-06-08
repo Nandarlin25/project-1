9f0375c098d030eaf2f901bf5428081aa0a8355e
Nandarlin25 <nandarlin2511@gmail.com>
2024-06-08 13:17:20 +0630

    first time

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily : {
         'poppins' : ["Poppins", "sans-serif"],
-        'paduak' : ["Padauk", "sans-serif"],
-        'poppins-mm' : ["Poppins","Padauk"]
+        'padauk' : ["Padauk", "sans-serif"],
+        'poppins-padauk' : ["Poppins","Padauk","sans-serif"]
       },
       colors : {
-        primary : "#E21B1B",
+        "primary" : "#E21B1B",
         "secondary" : "#A205F5",
       }
     },
